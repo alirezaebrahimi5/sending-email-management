f5ba286a210ec8510e2b6f6fbbb8853228a9709c
Alireza Ebrahimi <ar.ebrahimi96@gmail.com>
2023-10-31 14:45:33 +0330

    Create custom user model

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import CustomUser
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["email", "is_active", "is_staff", 'last_login']
+    sortable_by = ('last_login')
+    list_filter = ["is_active", "is_staff"]
+    search_fields = ('email',)
+
+
+admin.site.register(CustomUser, UserAdmin)
+
--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import CustomUserManager
 
-# Create your models here.
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("email address"), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
